0990d43fb2ea1eee9fc343e241b9461a7a02c58b
Unknown <trecy.christine.fournier@gmail.com>
2018-06-29 15:09:46 +0200

    Projet maven

---
+++ Maven/Musynamic/src/main/java/com/example/Musynamic/MusynamicApplication.java
@@ -0,0 +1,12 @@
+package com.example.Musynamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusynamicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusynamicApplication.class, args);
+	}
+}
---
+++ Maven/Musynamic/src/test/java/com/example/Musynamic/MusynamicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.Musynamic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MusynamicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
