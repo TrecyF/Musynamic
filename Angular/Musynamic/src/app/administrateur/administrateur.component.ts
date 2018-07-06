60667526a5425bf3468e2fbc57d6435d6cb9d0b7
David Ortega <david.ortega34000@gmail.com>
2018-07-06 18:48:17 +0200

    modif admin.component.ts

--- Angular/Musynamic/src/app/administrateur/administrateur.component.ts
+++ Angular/Musynamic/src/app/administrateur/administrateur.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Header } from '../header';
+import { Administrateur } from '../administrateur';
 
 @Component({
   selector: 'app-administrateur',
